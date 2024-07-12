7617050520a28f5bf2f6a4803ebc65f000a6e663
ErickSito20 <erick182022@gmail.com>
2024-07-12 15:01:57 -0500

    noche terminada

--- 1-css-javascript/app.js
+++ 1-css-javascript/app.js
@@ -14,10 +14,20 @@ function moversol(){
     sol1.style.backgroundColor='white'
 
 }
-function movernubes(){
-    nube1.style.animation= 'uno 3s linear ' 
+function movernube1(){
+    nube1.style.animation= 'uno 3s linear '  
+}
+function movernube2(){
     nube2.style.animation= 'dos 3s linear' 
+    
+}
+function movernube3(){
+   
     nube3.style.animation= 'tres 3s linear' 
+     
+}
+function movernube4(){
+ 
     nube4.style.animation= 'cuatro 3s linear' 
 }
 function Fondo1(){
@@ -25,6 +35,9 @@ function Fondo1(){
 
 }
 
-nubes.addEventListener('click', movernubes)
+nube1.addEventListener('click', movernube1)
+nube2.addEventListener('click', movernube2)
+nube3.addEventListener('click', movernube3)
+nube4.addEventListener('click', movernube4)
 guia.addEventListener('click',moversol)
 fondo.addEventListener('click',Fondo1)
