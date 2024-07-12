3322dc591ec221a5d7b1768cdc60062db4c43efb
ErickSito20 <erick182022@gmail.com>
2024-07-12 15:21:45 -0500

    noche con cambiar color terminada

--- 1-css-javascript/app.js
+++ 1-css-javascript/app.js
@@ -7,11 +7,13 @@ const nube1 = document.querySelector('.uno')
 const nube2 = document.querySelector('.dos')
 const nube3 = document.querySelector('.tres')
 const nube4 = document.querySelector('.cuatro')
-
-
+let e = 0 
 function moversol(){
+    
+        
     sol1.style.animation= 'sol 5s linear' 
     sol1.style.backgroundColor='white'
+   
 
 }
 function movernube1(){
@@ -31,8 +33,13 @@ function movernube4(){
     nube4.style.animation= 'cuatro 3s linear' 
 }
 function Fondo1(){
-    fondo.style.backgroundColor = 'blue' 
-
+    if( e=== 0 ) {
+        fondo.style.backgroundColor= '#95e6ff'
+e=1
+}else {
+    fondo.style.backgroundColor= 'blue'
+    e=0
+}
 }
 
 nube1.addEventListener('click', movernube1)
@@ -41,3 +48,4 @@ nube3.addEventListener('click', movernube3)
 nube4.addEventListener('click', movernube4)
 guia.addEventListener('click',moversol)
 fondo.addEventListener('click',Fondo1)
+
