f33f6b93bda1c08fe083330634d016945b42c8e1
ErickSito20 <erick182022@gmail.com>
2024-07-17 16:31:18 -0500

    terminado sin boton

---
+++ 2-poster-css-js/app.js
@@ -0,0 +1,35 @@
+const castillo = document.querySelector('.castle')
+const cielo = document.querySelector('.poster1')
+const boton = document.querySelector('.boton')
+const complemento = document.querySelector('.poster2')
+const hP= document.querySelector('.Hp')
+const potter = document.querySelector('.harry')
+const malo = document.querySelector('.daco')
+const logo = document.querySelector('.simbolo')
+
+function casti (){
+    castillo.style.animation = 'castle 3s 0s'
+    complemento.style.animation = 'castle 3s 0s'
+    cielo .style.animation = 'castle 3s 0s'
+
+    
+}
+function harry(){
+    potter.style.animation= 'mover 3s linear 1 0s, rotar 3s  linear 1 0s  '
+    potter.style.animation-composition= 'replace'  
+}
+function Hp(){
+    hP.style.animation= 'escale 3s  1 0s, escale2 3s  1 0s '  
+}
+function daco(){
+    malo.style.animation= 'moverd 3s linear 1 0s, rotard 3s  linear  1 0s '  
+    malo.style.animation-composition = 'replace' 
+}
+function simbolo(){
+    logo.style.animation= 'rotars 3s  0s '  
+}
+boton.addEventListener('click',casti)
+boton.addEventListener('click', Hp)
+boton.addEventListener('click', harry)
+boton.addEventListener('click', daco)
+boton.addEventListener('click', simbolo)
