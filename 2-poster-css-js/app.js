2481f32d9d9bb4e3fd0381a48fd334dc05c0e394
ErickSito20 <erick182022@gmail.com>
2024-07-17 21:19:08 -0500

    terminado

--- 2-poster-css-js/app.js
+++ 2-poster-css-js/app.js
@@ -1,22 +1,35 @@
-const castillo = document.querySelector('.castillo');
-const cielo = document.querySelector('.poster1');
-const buton = document.querySelector('.boton');
-const complemento = document.querySelector('.poster2');
-const hP= document.querySelector('.Hp');
-const potter = document.querySelector('.harry');
-const malo = document.querySelector('.daco');
-const logo = document.querySelector('.simbolo');
+const castillo = document.querySelector('.castillo')
+const cielo = document.querySelector('.poster1')
+const boton = document.querySelector('.boton')
+const complemento = document.querySelector('.poster2')
+const hP= document.querySelector('.Hp')
+const potter = document.querySelector('.harry')
+const malo = document.querySelector('.daco')
+const logo = document.querySelector('.simbolo')
 
-
-function activar (){
-    castillo.style.animation = "castle 3s 0s";
-    cielo.style.animation = "castle 3s 0s";
-    complemento.style.animation = "castle 3s 0s";
-    hP.style.animation = " escale 3s  1 0s, escale2 3s  1 0s";
-    potter.style.animation = "mover 3s linear 1 0s, rotar 3s  linear 1 0s";
-    malo.style.animation = "moverd 3s linear 1 0s, rotard 3s  linear  1 0s";
-    logo.style.animation = "rotars 3s  0s";
+function casti (){
+    castillo.style.animation = 'castle 3s 0s'
+    complemento.style.animation = 'castle 3s 0s'
+    cielo .style.animation = 'castle 3s 0s'
 
     
 }
-buton.addEventListener('click', activar)
+function harry(){
+    potter.style.animation= 'mover 3s linear 1 0s, rotar 3s  linear 1 0s  '
+      
+}
+function Hp(){
+    hP.style.animation= 'escale 3s  1 0s, escale2 3s  1 0s '  
+}
+function daco(){
+    malo.style.animation= 'moverd 3s linear 1 0s, rotard 3s  linear  1 0s '  
+     
+}
+function simbolo(){
+    logo.style.animation= 'rotars 3s  0s '  
+}
+boton.addEventListener('click',casti)
+boton.addEventListener('click', Hp)
+boton.addEventListener('click', harry)
+boton.addEventListener('click', daco)
+boton.addEventListener('click', simbolo)
