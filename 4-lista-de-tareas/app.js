deb895cb911ed3e6016057017b5d832f4045d31d
ErickSito20 <erick182022@gmail.com>
2024-07-24 15:45:55 -0500

    terminado

--- 4-lista-de-tareas/app.js
+++ 4-lista-de-tareas/app.js
@@ -3,43 +3,56 @@ const btnBorrar = document.querySelector('.borrar')
 const btnReset = document.querySelector('.borrartodo')
 
 
-const lista = document.querySelector('#listalibros')
+const list = document.querySelector('#listalibros')
 const input= document.querySelector('#Lb')
 
 
-const cuadrouno = document.querySelector('#cuadros')
-const container=  document.querySelector('.cero')
-let n = 1 
+const cuadrouno = document.querySelector('.cuadros')
+const container=  document.querySelector('#secundario')
+const tareas = document.querySelector('.tareas')
+
 function crear(){
     
-    const libro = document.createElement('div')
-    libro.innerText = input.value
-    libro.classList.add('libros')
-    lista.appendChild(libro)
-    const divbotones = document.createElement('div')
-    cuadrouno.appendChild(divbotones)
-    divbotones.classList.add('botondiv')
-    const check2 =  document.createElement('button')
-    check2.innerText = 'completar'
-
-    divbotones.appendChild(check2)
-
-    check2.classList.add('otros')
-const check1 =  document.createElement('button')
-    check1.innerText = 'borrar'
-    check1.classList.add('otros')
-    divbotones.appendChild(check1)
+    const tarea = document.createElement('div')
+    container.appendChild(tarea)
+    tarea.classList.add('tarea')
     
-
+    const lista = document.createElement('div')
+    tarea.appendChild(lista)
+    lista.classList.add('libros')
+    lista.innerText = input.value
+    const divbotones = document.createElement('div')
+    divbotones.classList.add('botondiv')
+    tarea.appendChild(divbotones)
+    const boton1 = document.createElement('button')
+    boton1.classList.add('otros')
+    boton1.innerText = 'borrar'
+    divbotones.appendChild(boton1)
+    const boton2 = document.createElement('button')
+    boton2.classList.add('otros')
+    boton2.innerText = 'completar'
+    divbotones.appendChild(boton2)
     
 }
 
 function borrarElemento (e){ 
     if(e.target.innerText == 'borrar'){
-        container.removeChild(e.target.parentElement);
+        console.log(e.target.parentElement.parentElement)
+        container.removeChild(e.target.parentElement.parentElement);
+    }
+
+}
+function borrarPantalla (){
+    container.innerHTML = '';
+}
+function completarTarea (e){ 
+    if(e.target.innerText == 'completar'){
+        
+        e.target.parentElement.parentElement.firstElementChild.style.textDecoration = 'line-through';    
     }
 }
 
-
 btnNovo.addEventListener('click',crear)
-btnReset,addEventListener('click', borrarPantalla)
+container.addEventListener('click',borrarElemento)
+btnReset.addEventListener('click',borrarPantalla)
+container.addEventListener('click',completarTarea)
