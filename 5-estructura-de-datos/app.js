bd305d2e63712f4701511f6b1f62b4950dfdb160
ErickSito20 <erick182022@gmail.com>
2024-07-30 15:39:58 -0500

    terminado

--- 5-estructura-de-datos/app.js
+++ 5-estructura-de-datos/app.js
@@ -10,11 +10,11 @@ card.classList.add('card')
             <img src="${element.imagen}" alt="">
         </div>
         <div id="dos">
-            <div id="id"><h3 class="title"> ${element.id}</h3></div>
-            <div id="name"><h3 class="title">nombre: ${element.nombre}</h3></div>
-            <div id="artista"><h3 class="title">artista: ${element.artista}</h3></div>
-            <div id="year"><h3 class="title">año: ${element.year}</h3></div>
-            <div id="genero"><h3 class="title">género: ${element.genero}</h3></div>
+            <div id="id"><h3 class="title1"> ${element.id}</h3></div>
+            <div id="name"><h3 class="title"> nombre: ${element.nombre}</h3></div>
+            <div id="artista"><h3 class="title"> artista: ${element.artista}</h3></div>
+            <div id="year"><h3 class="title"> año: ${element.year}</h3></div>
+            <div id="genero"><h3 class="title"> género: ${element.genero}</h3></div>
         </div>
     </section>
     `
